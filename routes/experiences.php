50b28c879a908f5573d440962501a7a4949f6bed
clement tribouillard <clement.tribouillard@gmail.com>
2025-04-17 14:31:03 -0400

    fix bug: get a WorkExperience object

--- routes/experiences.php
+++ routes/experiences.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\WorkExperienceController;
 // Routes pour les actions CRUD du WorkExperienceController
 Route::prefix('experiences')->group(function () {
     Route::get('/list', [WorkExperienceController::class, 'index'])->name('experiences.index');
-    Route::get('/{experience}', [WorkExperienceController::class, 'show'])->name('experiences.show');
-    Route::put('/{experience}', [WorkExperienceController::class, 'update'])->name('experiences.update');
-    Route::delete('/{experience}', [WorkExperienceController::class, 'destroy'])->name('experiences.destroy');
+    Route::get('/{workExperience}', [WorkExperienceController::class, 'show'])->name('experiences.show');
+    Route::put('/{workExperience}', [WorkExperienceController::class, 'update'])->name('experiences.update');
+    Route::delete('/{workExperience}', [WorkExperienceController::class, 'destroy'])->name('experiences.destroy');
 });
 
 // Route pour afficher la liste de messages
