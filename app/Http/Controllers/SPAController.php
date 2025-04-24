0001d4745192fdb25510eebf4da7ad21345164ce
clement tribouillard <clement.tribouillard@gmail.com>
2025-04-23 23:16:41 -0400

    SPAController

---
+++ app/Http/Controllers/SPAController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\WorkExperience;
+use App\Models\Education;
+use App\Models\Hobby;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class SPAController extends Controller
+{
+
+    const CACHE_KEY = '';
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        if (Cache::has(self::CACHE_KEY)) {
+            $value = Cache::get(self::CACHE_KEY);
+        } else {
+            $value = [
+                'experiences' => WorkExperience::orderByRaw('end_at IS NOT NULL, end_at DESC')->get(),
+                'schools' => Education::orderByRaw('date IS NOT NULL, date DESC')->get(),
+                'hobbies' => Hobby::orderBy('order ASC')->get(),
+            ];
+            Cache::add('key', 0, now()->addHours(12));
+        }
+        return response()->json($value);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\SPAController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -18,3 +19,7 @@ require __DIR__.'/message.php';
 require __DIR__.'/experiences.php';
 require __DIR__.'/education.php';
 require __DIR__.'/hobbies.php';
+
+Route::prefix('api/spa')->group(function () {
+    Route::get('/list', [SPAController::class, 'index'])->name('spa.index');
+});
