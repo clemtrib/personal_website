052c118741c6e829130a3c7e89e80b3218d9da6f
clement tribouillard <clement.tribouillard@gmail.com>
2025-04-29 13:56:13 -0400

    format experiences block

--- resources/js/components/front/ExperienceSection.vue
+++ resources/js/components/front/ExperienceSection.vue
@@ -18,19 +18,19 @@ defineProps < {
         <h2 class="text-3xl font-bold mb-10 border-b-2 border-green-400 inline-block">Expériences & Projets</h2>
         <div class="space-y-10">
             <div v-for="experience in experiences" :key="experience.id">
-                <h3 class="text-xl font-semibold text-white">{{ experience.job }} - {{ experience.company }} <small>{{ experience.location }}</small></h3>
-                <p class="text-[#8892b0] mt-2">
-                    <p class="text-sm text-gray-600 dark:text-gray-400">
-                        Du {{ new Date(experience.begin_at).toLocaleDateString() }}
-                        <template v-if="experience.end_at">
-                            au {{ new Date(experience.end_at).toLocaleDateString() }}
-                        </template>
-                        <template v-else>
+                <h3 class="text-1xl">
+                    <span class="text-green-400">
+                        De {{ new Date(experience.begin_at).toLocaleDateString('fr-FR', { month: 'long', year: 'numeric' }) }}
+                        <span v-if="experience.end_at">
+                            à {{ new Date(experience.end_at).toLocaleDateString('fr-FR', { month: 'long', year: 'numeric' }) }}
+                        </span>
+                        <span v-else>
                             à aujourd'hui
-                        </template>
-                    </p>
-                </p>
-                <p class="text-sm text-gray-600 dark:text-gray-400" v-html="experience.description"></p>
+                        </span>
+                    </span> <small>- {{ experience.location }}</small>
+                </h3>
+                <h2 class="text-xl font-semibold text-white">{{ experience.job }} @ <span class="text-green-400">{{ experience.company }}</span></h2>
+                <div class="block w-full md:w-1/2 text-1xl text-gray-600 dark:text-gray-400 text-justify prose prose-invert max-w-none" v-html="experience.description"></div>
             </div>
         </div>
     </section>
