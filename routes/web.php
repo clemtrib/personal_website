af65cf79d10fdf5913e2f3b08cc8457681bb531c
clement tribouillard <clement.tribouillard@gmail.com>
2025-05-02 16:03:56 -0400

    ovh: trigger migrations

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\SPAController;
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -24,4 +26,23 @@ require __DIR__.'/pages.php';
 
 Route::prefix('api/spa')->group(function () {
     Route::get('/list', [SPAController::class, 'index'])->name('spa.index');
+});
+
+
+
+Route::get('/run-migrations', function (\Illuminate\Http\Request $request) {
+    // Protection par token
+    $token = $request->query('token');
+
+    if ($token !== env('MIGRATION_WEBHOOK_TOKEN')) {
+        abort(403, 'Unauthorized');
+    }
+
+    // Exécute les migrations en production (sans confirmation)
+    Artisan::call('migrate', ['--force' => true]);
+
+    return response()->json([
+        'status' => 'success',
+        'output' => Artisan::output()
+    ]);
 });
