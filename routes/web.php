747f0f36c9c936c76c60a6ca38871491764ffae4
clement tribouillard <clement.tribouillard@gmail.com>
2025-05-02 23:35:39 -0400

    deploy: clear cache

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
 
-require __DIR__.'/message.php';
-require __DIR__.'/experiences.php';
-require __DIR__.'/education.php';
-require __DIR__.'/hobbies.php';
-require __DIR__.'/skills.php';
-require __DIR__.'/pages.php';
+require __DIR__ . '/message.php';
+require __DIR__ . '/experiences.php';
+require __DIR__ . '/education.php';
+require __DIR__ . '/hobbies.php';
+require __DIR__ . '/skills.php';
+require __DIR__ . '/pages.php';
 
 Route::prefix('api/spa')->group(function () {
     Route::get('/list', [SPAController::class, 'index'])->name('spa.index');
@@ -45,4 +45,19 @@ Route::get('/run-migrations', function (\Illuminate\Http\Request $request) {
         'status' => 'success',
         'output' => Artisan::output()
     ]);
+});
+
+Route::get('/clear-cache', function (\Illuminate\Http\Request $request) {
+    // Protection par token
+    $token = $request->query('token');
+
+    if ($token !== env('MIGRATION_WEBHOOK_TOKEN')) {
+        abort(403, 'Unauthorized');
+    }
+
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return 'Caches cleared';
 });
