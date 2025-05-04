8ef92d78e20feb9f3b7b3fd667c61ba86955f21f
clement tribouillard <clement.tribouillard@gmail.com>
2025-05-03 23:18:51 -0400

    fix bug deploy

--- database/migrations/2025_05_04_022152_update_date_fields_in_education_work_experiences_and_projects.php
+++ database/migrations/2025_05_04_022152_update_date_fields_in_education_work_experiences_and_projects.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,12 +13,21 @@ return new class extends Migration
     public function up(): void
     {
         // Education table
+        DB::table('education')
+            ->whereRaw("LENGTH(date) != 10 OR date NOT LIKE '____-__-__'")
+            ->update(['date' => null]);
         Schema::table('education', function (Blueprint $table) {
             // Modifier la colonne 'date' en type date nullable
             $table->date('date')->nullable()->change();
         });
 
         // Work experiences table
+        DB::table('work_experiences')
+            ->whereRaw("LENGTH(begin_at) != 10 OR begin_at NOT LIKE '____-__-__'")
+            ->update(['begin_at' => null]);
+        DB::table('work_experiences')
+            ->whereRaw("LENGTH(end_at) != 10 OR end_at NOT LIKE '____-__-__'")
+            ->update(['end_at' => null]);
         Schema::table('work_experiences', function (Blueprint $table) {
             // Modifier 'begin_at' et 'end_at' en date nullable
             $table->date('begin_at')->nullable()->change();
@@ -25,6 +35,12 @@ return new class extends Migration
         });
 
         // Projects table
+        DB::table('projects')
+            ->whereRaw("LENGTH(begin_at) != 10 OR begin_at NOT LIKE '____-__-__'")
+            ->update(['begin_at' => null]);
+        DB::table('projects')
+            ->whereRaw("LENGTH(end_at) != 10 OR end_at NOT LIKE '____-__-__'")
+            ->update(['end_at' => null]);
         Schema::table('projects', function (Blueprint $table) {
             // Modifier 'begin_at' et 'end_at' en date nullable
             $table->date('begin_at')->nullable()->change();
