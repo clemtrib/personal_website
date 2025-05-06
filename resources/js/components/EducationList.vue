2003b24ba91cd7e088336780e62cac49ede8c3c2
clement tribouillard <clement.tribouillard@gmail.com>
2025-05-06 14:48:07 -0400

    affichage formation

--- resources/js/components/EducationList.vue
+++ resources/js/components/EducationList.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed, ref, defineExpose } from 'vue';
 import { useForm } from '@inertiajs/vue3';
 import { Pencil, Eraser } from 'lucide-vue-next';
 import { Link } from '@inertiajs/vue3';
@@ -29,23 +28,20 @@ const openDeleteModal = (id: number) => {
             <ul class="space-y-4">
                 <li v-for="education in educations" :key="education.id" class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow">
                     <!-- Contenu expérience... -->
-
-                    <h3 class="font-semibold">{{ education.school }} <small>{{ education.location }}</small></h3>
+                    <h3 class="font-semibold">
+                        <span class="text-sm text-gray-600 dark:text-green-400">
+                                {{ new Date(education.date).toLocaleDateString() }}
+                            </span> - {{ education.school }}</h3>
                     <p class="text-sm text-gray-600 dark:text-gray-400" v-html="education.graduation"></p>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">
-                        {{ new Date(education.date).toLocaleDateString() }}
-                    </p>
-                    <p class="mt-2 prose prose-invert max-w-none" v-html="education.description"></p>
-
                     <div class="flex gap-2 mt-4">
                         <Link :href="route('education.edit', education.id)" class="flex items-center gap-2 px-3 py-2 text-sm bg-blue-100 dark:bg-blue-900/30 hover:bg-blue-200 dark:hover:bg-blue-900/50 rounded-lg transition">
                         <Pencil class="h-4 w-4" /> Modifier
                         </Link>
 
                         <button @click="openDeleteModal(education.id)" class="flex items-center gap-2 px-3 py-2 text-sm bg-red-100 dark:bg-red-900/30 hover:bg-red-200 dark:hover:bg-red-900/50 rounded-lg transition">
-                      <Eraser class="h-4 w-4" />
-                      Supprimer
-                    </button>
+                              <Eraser class="h-4 w-4" />
+                              Supprimer
+                            </button>
                     </div>
                 </li>
             </ul>
