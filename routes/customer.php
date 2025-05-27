20b337036179af6844b28533fac89874e77d9e4c
clement tribouillard <clement.tribouillard@gmail.com>
2025-05-26 23:26:26 -0400

    CustomerController

---
+++ routes/customer.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\CustomerController;
+
+Route::prefix('dashboard/customers')->middleware('auth')->group(function () {
+    Route::get('/list', [CustomerController::class, 'index'])->name('customers');
+    Route::get('/create', function () {
+        return Inertia::render('CustomersForm');
+    })->name('customers.create');
+    Route::post('/create', [CustomerController::class, 'store'])->name('customers.store');
+    Route::get('/{hobby}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+    Route::put('/{hobby}', [CustomerController::class, 'update'])->name('customers.update');
+    Route::delete('/{hobby}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+    Route::post('/reorder', [CustomerController::class, 'reorder'])->name('customers.reorder');
+});
